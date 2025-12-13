8e674f8229ff2dc69af2486891c16950a9d735cf
praful2431 <praful.math06@gmail.com>
2025-12-13 17:05:05 +0530

    teams done. besides drivers send off

--- sem1SEE-F1Project/teams.js
+++ sem1SEE-F1Project/teams.js
@@ -12,184 +12,113 @@ for(i = 0; i < 5; i++){
     }
 }
 
-let car;
+let curCar = null;
+cars = ["mclaren", "ferarri", "mercedes", "redbull", "aston", "racingbull", "williams", "alpine", "haas", "sauber"];
 
-function mclaren(){
-    car = document.getElementById("r1c1");
+function carClickOFF(car, bg){
+
+    car.classList.toggle("active");
+
+            car.style.backgroundColor = (bg + "0.73);");
+            car.style.boxShadow = "";
+
+            car.style.transform = "scale(1)";
+}
+
+function carClick(car, newBg, glow, bg){
+
     car.classList.toggle("active");
 
     if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#f47600ed";
-            car.style.boxShadow = "0 0 25px #ff7b00";
+        car.style.backgroundColor = newBg;
+        car.style.boxShadow = `0 0 25px ${glow}`;
 
-            car.style.transform = "scale(1.1)";
-
+        car.style.transform = "scale(1.1)";
     } else {
-            car.style.backgroundColor = "#F47600ba";
+            car.style.backgroundColor = bg;
             car.style.boxShadow = "";
 
             car.style.transform = "scale(1)";
     }
+}
+
+
+function carReset(curCar){
+    carList = document.getElementsByClassName("team-card");
+
+    let car;
+
+    for(i = 0; i < 10; i++){
+        
+        if(carList[i].classList[1] == curCar){
+            car = carList[i].id;
+            car = document.getElementById(car);
+            break;
+        }
+    }
+
+    carClickOFF(car, car.style.backgroundColor.split("0.")[0]);
+}
+
+
+function mclaren(){
+    carClick(document.getElementById("r1c1"), "#f47600ed", "#ff7b00", "#F47600ba");
 }
 
 function ferarri(){
-    car = document.getElementById("r1c2");
-    car.classList.toggle("active");
-
-    if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#ED1131ed";
-            car.style.boxShadow = "0 0 25px #ff0026ff";
-
-            car.style.transform = "scale(1.1)";
-
-    } else {
-            car.style.backgroundColor = "#ED1131ba";
-            car.style.boxShadow = "";
-
-            car.style.transform = "scale(1)";
-    }
+    carClick(document.getElementById("r1c2"), "#ED1131ed", "#ff0026ff", "#ED1131ba");
 }
 
 function mercedes(){
-    car = document.getElementById("r2c1");
-    car.classList.toggle("active");
-
-    if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#C8CCCEed";
-            car.style.boxShadow = "0 0 25px #c8ccceff";
-
-            car.style.transform = "scale(1.1)";
-
-    } else {
-            car.style.backgroundColor = "#C8CCCEba";
-            car.style.boxShadow = "";
-
-            car.style.transform = "scale(1)";
-    }
+    carClick(document.getElementById("r2c1"), "#C8CCCEed", "#c8ccceff", "#C8CCCEba");
 }
 
 function redbull(){
-    car = document.getElementById("r2c2");
-    car.classList.toggle("active");
-
-    if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#4781D7ed";
-            car.style.boxShadow = "0 0 25px #4590ffff";
-
-            car.style.transform = "scale(1.1)";
-
-    } else {
-            car.style.backgroundColor = "#4781D7ba";
-            car.style.boxShadow = "";
-
-            car.style.transform = "scale(1)";
-    }
+    carClick(document.getElementById("r2c2"), "#4781D7ed", "#4590ffff", "#4781D7ba");
 }
 
 function aston(){
-    car = document.getElementById("r3c1");
-    car.classList.toggle("active");
-
-    if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#037a68ff";
-            car.style.boxShadow = "0 0 25px #00d1b1ff";
-
-            car.style.transform = "scale(1.1)";
-
-    } else {
-            car.style.backgroundColor = "#037A68ba";
-            car.style.boxShadow = "";
-
-            car.style.transform = "scale(1)";
-    }
+    carClick(document.getElementById("r3c1"), "#037a68ff", "#00d1b1ff", "#037A68ba");
 }
 
 function racingbull(){
-    car = document.getElementById("r3c2");
-    car.classList.toggle("active");
-
-    if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#6C98FFed";
-            car.style.boxShadow = "0 0 25px #3a75ffff";
-
-            car.style.transform = "scale(1.1)";
-
-    } else {
-            car.style.backgroundColor = "#6C98FFba";
-            car.style.boxShadow = "";
-
-            car.style.transform = "scale(1)";
-    }
+    carClick(document.getElementById("r3c2"), "#6C98FFed", "#3a75ffff", "#6C98FFba");
 }
 
 function williams(){
-    car = document.getElementById("r4c1");
-    car.classList.toggle("active");
-
-    if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#1868DBed";
-            car.style.boxShadow = "0 0 25px #006affff";
-
-            car.style.transform = "scale(1.1)";
-
-    } else {
-            car.style.backgroundColor = "#1868DBba";
-            car.style.boxShadow = "";
-
-            car.style.transform = "scale(1)";
-    }
+    carClick(document.getElementById("r4c1"), "#1868DBed", "#006affff", "#1868DBba");
 }
 
 function alpine(){
-    car = document.getElementById("r4c2");
-    car.classList.toggle("active");
-
-    if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#00A1E8ed";
-            car.style.boxShadow = "0 0 25px #00b3ffff";
-
-            car.style.transform = "scale(1.1)";
-
-    } else {
-            car.style.backgroundColor = "#00A1E8ba";
-            car.style.boxShadow = "";
-
-            car.style.transform = "scale(1)";
-    }
+    carClick(document.getElementById("r4c2"), "#00A1E8ed", "#00b3ffff", "#00A1E8ba");
 }
 
 function haas(){
-    car = document.getElementById("r5c1");
-    car.classList.toggle("active");
-
-    if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#9C9FA2ed";
-            car.style.boxShadow = "0 0 25px #9c9fa2ff";
-
-            car.style.transform = "scale(1.1)";
-
-    } else {
-            car.style.backgroundColor = "#9C9FA2ba";
-            car.style.boxShadow = "";
-
-            car.style.transform = "scale(1)";
-    }
+    carClick(document.getElementById("r5c1"), "#9C9FA2ed", "#9c9fa2ff", "#9C9FA2ba");
 }
 
 function sauber(){
-    car = document.getElementById("r5c2");
-    car.classList.toggle("active");
-
-    if(car.classList.contains("active")) {
-            car.style.backgroundColor = "#01C00Eed";
-            car.style.boxShadow = "0 0 25px #00e30fff";
-
-            car.style.transform = "scale(1.1)";
-
-    } else {
-            car.style.backgroundColor = "#01C00Eba";
-            car.style.boxShadow = "";
-
-            car.style.transform = "scale(1)";
-    }
+    carClick(document.getElementById("r5c2"), "#01C00Eed", "#00e30fff", "#01C00Eba");
 }
+
+
+let click = null;
+document.addEventListener("click", function(e) {
+    
+    click = e.srcElement.classList[1];
+    
+    if(cars.includes(click)){
+        if (curCar == null) {
+            curCar = e.srcElement.classList[1];
+        } else if(click == curCar){
+            console.log("Changeing page to drivers");
+        } else {
+            carReset(curCar);
+            curCar = click;
+        }
+    } else {
+        carReset(curCar);
+        curCar = null;
+    }
+
+});
