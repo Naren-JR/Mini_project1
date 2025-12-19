ba101b548ab28877140cc7c5c1d48fd3e6d3f039
praful2431 <praful.math06@gmail.com>
2025-12-19 20:05:31 +0530

    js form fix

--- sem1SEE-F1Project/visit.js
+++ sem1SEE-F1Project/visit.js
@@ -18,12 +18,39 @@ function validateForm() {
     event.preventDefault();
 
     let name = document.getElementById("name").value.trim();
-    let email = document.getElementById("email").value.trim();
-    let contact = document.getElementById("contact").value.trim();
-    let gender = document.getElementById("gender").value.trim();
-    let dob = document.getElementById("dob");
+    if (!name) return alert("Name is required.");
 
+    let email = document.getElementById("email").value.trim();
+    let emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!email.match(emailPattern)) return alert("Enter a valid email address.");
+
+    let contact = document.getElementById("contact").value.trim();
     if (contact.length != 10) return alert("Enter a valid phone number.");
+
+    let gender = document.getElementById("gender").value.trim();
+    if (!gender) return alert("Gender is required.");
+
+    let dob = document.getElementById("dob").value;
+    if (!dob) return alert("Date of Birth is required.");
+
+    let dept = document.querySelectorAll("input[name='department']:checked");
+    if (dept.length === 0) return alert("Select at least one Program Department.");
+
+    let vDate = document.getElementById("vDate").value;
+    if (!vDate) return alert("Please select your preferred date.");
+
+    let duration = document.querySelector("input[name='duration']:checked");
+    if (!duration) return alert("Select a tour duration.");
+
+    let govID = document.getElementsByClassName("myID")[0].files.length;
+    if (!govID) return alert("Upload a valid government ID.");
+
+    let orgName = document.getElementById("orgName").value.trim();
+    if (!orgName) return alert("Enter organization name.");
+
+    let clgID = document.getElementsByClassName("myID")[1].files.length;
+    if (!clgID) return alert("Upload organization ID.");
+
 
     // create message
     let done = document.createElement("div");
